4f4537c79913e0b48f72463a1a30bae6e84b64ed
Sara Shamsher <shamsher.sara@gmail.com>
2021-08-19 12:47:28 +0530

    first commit

---
+++ trainer.py
@@ -0,0 +1,18 @@
+from chatbot import chatbot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+trainer = ChatterBotCorpusTrainer(chatbot)
+""""
+trainer.train(
+    "chatterbot.corpus.english.greetings",
+    "chatterbot.corpus.english.communications"
+    "./knowledge/emotion.yml",
+    "./knowledge/food.yml",
+    "./knowledge/money.yml",
+    "./knowledge/history.yml"
+)
+"""
+def brain(user_input):
+    response=chatbot.get_response(user_input)
+    return response
+    
